ced276b9d3163db9032d316fceb41978c7af6173
Ebuka321 <corneliusemmanuel55@gmail.com>
2026-01-09 14:54:53 -0800

    updated to the correct import statement

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Header from "./pages/components/Header";
+import Header from "./components/Header";
 
 function App() {
   return (
