be9c9e46ffcd6321dd76e49628f57bc6a0d22ee3
Ebuka321 <corneliusemmanuel55@gmail.com>
2026-04-30 12:00:00 -0700

    feat: add host routes with auth protection (Dashboard, Income, Reviews, HostVans, HostVanDetail)

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,24 @@ import About from "./pages/About";
 import Vans, { loader as vansLoader } from "./components/Vans";
 import VanDetail, { loader as vanDetailLoader } from "./pages/VanDetail";
 import LayoutRoute from "./pages/LayoutRoute";
+import Dashboard, { loader as dashboardLoader } from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
+import HostLayout from "./pages/HostLayout";
+import Hostvans, { loader as hostVansLoader } from "./pages/Host/Hostvans";
+import HostvanDetail, {
+  loader as hostVanDetailLoader,
+} from "./pages/Host/HostvanDetail";
+import HostVanPricing from "./pages/Host/HostVanPricing";
+import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import HostVanInfo from "./pages/Host/HostVanInfo";
 import NotFound from "./components/NotFound";
 import Error from "./components/Error";
 import Login, {
   loader as loginLoader,
   action as loginAction,
 } from "./pages/Login";
+import { requireAuth } from "./utils";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -40,6 +52,56 @@ const router = createBrowserRouter(
         loader={vanDetailLoader}
         errorElement={<Error />}
       />
+      <Route
+        path="host"
+        element={<HostLayout />}
+        loader={({ request }) => requireAuth(request)}
+      >
+        <Route
+          index
+          element={<Dashboard />}
+          loader={dashboardLoader}
+          errorElement={<Error />}
+        />
+        <Route
+          path="income"
+          element={<Income />}
+          loader={async ({ request }) => await requireAuth(request)}
+        />
+        <Route
+          path="reviews"
+          element={<Reviews />}
+          loader={async ({ request }) => await requireAuth(request)}
+        />
+        <Route
+          path="vans"
+          element={<Hostvans />}
+          loader={hostVansLoader}
+          errorElement={<Error />}
+        />
+        <Route
+          path="vans/:id"
+          element={<HostvanDetail />}
+          loader={hostVanDetailLoader}
+          errorElement={<Error />}
+        >
+          <Route
+            index
+            element={<HostVanInfo />}
+            loader={async ({ request }) => await requireAuth(request)}
+          />
+          <Route
+            path="pricing"
+            element={<HostVanPricing />}
+            loader={async ({ request }) => await requireAuth(request)}
+          />
+          <Route
+            path="photos"
+            element={<HostVanPhotos />}
+            loader={async ({ request }) => await requireAuth(request)}
+          />
+        </Route>
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Route>,
   ),
